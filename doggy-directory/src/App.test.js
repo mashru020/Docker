abbd48ca8b215be6b80e6bb0812531a6803d6667
mashru020 <mashrurrahman16@gmail.com>
2023-05-10 11:26:05 +0600

    doggy_directory for jest test

---
+++ doggy-directory/src/App.test.js
@@ -0,0 +1,21 @@
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import mockFetch from "./mocks/mockFetch";
+import App from './App';
+
+beforeEach(() => {
+   jest.spyOn(window, "fetch").mockImplementation(mockFetch);
+})
+
+afterEach(() => {
+   jest.restoreAllMocks()
+});
+
+test('renders the landing page', async () => {
+  render(<App />);
+  
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
+  expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
+  expect(await screen.findByRole("option", { name: "husky"})).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
+  expect(screen.getByRole("img")).toBeInTheDocument();
+}); 
